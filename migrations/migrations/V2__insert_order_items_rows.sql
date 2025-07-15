e2c3574306b8769b2cd80342df7e2607eff62070
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2025-07-16 01:24:51 +0200

    feat: Add V2 migration to insert order items
    This commit introduces a new migration file (V2) that inserts additional rows into the  table.

---
+++ migrations/migrations/V2__insert_order_items_rows.sql
@@ -0,0 +1,3 @@
+INSERT INTO order_items (order_item_id, order_id, product_id, quantity) VALUES
+(10, 108, 2, 2), -- Corresponds to Michael Garcia's order
+(11, 112, 4, 2); -- Corresponds to Laura Moore's order
